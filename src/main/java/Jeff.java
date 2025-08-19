99999c28763cc69faade19506ca9159b98a125a0
JonChng <jonathanchngyk@gmail.com>
2025-08-19 16:58:51 +0800

    Add task listing functionality
    Continously takes in tasks from user and adds to list. Limited at 100 tasks.

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -6,26 +6,32 @@ public class Jeff {
             "What can I do for you?"
         );
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (true) {
             String input = System.console().readLine();
 
-            if (input.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < tasks.length; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else if (input.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println("I can do " + input + " for you?\nSure I can!");
-
-            System.out.println("________________________________________");
-            System.out.println(
-                "How are you feeling?\n" 
-            );
-
-            String feeling = System.console().readLine();
-            if (feeling.equalsIgnoreCase("bye")) {
-                break;
+            else {
+                if (taskCount < tasks.length) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("______________________________");
+                    System.out.println("added: " + input);
+                    System.out.println("______________________________");
+                } else {
+                    System.out.println("Task list is full!");
+                }
             }
-            System.out.println("I AM FEELING " + feeling + " TOO!\n");
-
-            System.out.println("________________________________________");
 
         }
         System.out.println("Bye! Hope to you see you again soon!\n");
