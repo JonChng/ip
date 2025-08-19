e5463cc750d7e857fa52255654f8aa8072002501
JonChng <jonathanchngyk@gmail.com>
2025-08-19 22:50:26 +0800

    Update to use ArrayList
    Remove singular array. Implementation of the ArrayList collection for ease of use.

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jeff {
     public static void main(String[] args) {
@@ -9,8 +10,7 @@ public class Jeff {
             "What can I do for you?"
         );
 
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         
         
             while (sc.hasNextLine()) {
@@ -20,9 +20,9 @@ public class Jeff {
                     String cmd = split[0];
 
                     if (cmd.equalsIgnoreCase("list")) {
-                        for (int i = 0; i < tasks.length; i++) {
-                            if (tasks[i] != null) {
-                                System.out.println((i + 1) + ". " + tasks[i]);
+                        for (int i = 0; i < tasks.size(); i++) {
+                            if (tasks.get(i) != null) {
+                                System.out.println((i + 1) + ". " + tasks.get(i));
                             }
                         }
                     } else if (cmd.equalsIgnoreCase("bye")) {
@@ -37,66 +37,56 @@ public class Jeff {
                         else if (cmd.equalsIgnoreCase("mark")) {
 
                         int length = Integer.parseInt(split[1]);
-                        tasks[length - 1].markAsDone();
+                        tasks.get(length - 1).markAsDone();
                         System.out.println("Task marked as done!");
-                        System.out.println(tasks[length - 1]);
+                        System.out.println(tasks.get(length - 1));
                         System.out.println("______________________________");
 
                     } else if (cmd.equalsIgnoreCase("unmark")) {
                         int length = Integer.parseInt(split[1]);
-                        tasks[length - 1].undo();
+                        tasks.get(length - 1).undo();
                         System.out.println("Task marked as undone!");
-                        System.out.println(tasks[length - 1]);
+                        System.out.println(tasks.get(length - 1));
                     } else if (cmd.equalsIgnoreCase("delete")) {
 
                         int idx = (Integer.parseInt(split[1]) - 1); // This would be the index to be deleted.
 
-                        if (idx < 0 || idx >= taskCount) {
+                        if (idx < 0 || idx >= tasks.size()) {
                             throw new JeffException("Invalid task number. Please try again.");
                         }
-
-                        for (int i = idx; i < taskCount - 1; i++) {
-                            tasks[i] = tasks[i + 1];
-                        }
-
-                        tasks[taskCount - 1] = null; 
-                        taskCount--;
+                        tasks.remove(idx);
                         System.out.println("Task has been deleted.");
-                        System.out.println("You now have " + taskCount + " tasks in the list.");
+                        System.out.println("You now have " + tasks.size() + " tasks in the list.");
 
                         
                     }
                     else {
-                        if (taskCount < tasks.length) {
+                        
 
-                            switch (cmd.toLowerCase()) {
-                                case "todo":
-                                    tasks[taskCount] = new Todo(split[1]);
-                                    break;
-                                case "deadline":
-                                    String[] deadlineParts = split[1].split("/by", 2);
-                                    tasks[taskCount] = new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim());
-                                    break;
-                                case "event":
-                                    String[] eventParts = split[1].split("/at", 2);
-                                    tasks[taskCount] = new Event(eventParts[0].trim(), eventParts[1].trim());
-                                    break;
-                            
-                            }
-
-                            
-                            taskCount++;
-                            System.out.println("______________________________");
-                            System.out.println("Task has been added: " + input);
-                            System.out.println("You now have " + taskCount + " tasks in the list.");
-                            System.out.println("______________________________");
-                        } else {
-                            System.out.println("Task list is full!");
+                        switch (cmd.toLowerCase()) {
+                            case "todo":
+                                tasks.add(new Todo(split[1]));
+                                break;
+                            case "deadline":
+                                String[] deadlineParts = split[1].split("/by", 2);
+                                tasks.add(new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim()));
+                                break;
+                            case "event":
+                                String[] eventParts = split[1].split("/at", 2);
+                                tasks.add(new Event(eventParts[0].trim(), eventParts[1].trim()));
+                                break;
+                        
                         }
-                    }
-                    
 
+                        
+                        System.out.println("______________________________");
+                        System.out.println("Task has been added: " + input);
+                        System.out.println("You now have " + tasks.size() + " tasks in the list.");
+                        System.out.println("______________________________");
+                    } 
                 }
+                
+
             catch (JeffException e) {
                 System.out.println(e.getMessage());
             } 
