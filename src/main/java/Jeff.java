796314142055a598b3989720c2006d2797ee13ab
JonChng <jonathanchngyk@gmail.com>
2025-08-19 16:52:30 +0800

    Add Echo functionality for chatbot.
    Allows bot to repeat users input with some personality.

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -5,8 +5,30 @@ public class Jeff {
         System.out.println(
             "What can I do for you?"
         );
-        System.out.println("________________________________________");
 
+        while (true) {
+            String input = System.console().readLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("I can do " + input + " for you?\nSure I can!");
+
+            System.out.println("________________________________________");
+            System.out.println(
+                "How are you feeling?\n" 
+            );
+
+            String feeling = System.console().readLine();
+            if (feeling.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("I AM FEELING " + feeling + " TOO!\n");
+
+            System.out.println("________________________________________");
+
+        }
         System.out.println("Bye! Hope to you see you again soon!\n");
+
     }
 }
