fa00fb4d39b25ac7e86bd8ce07c03bbb7e63f3b3
JonChng <jonathanchngyk@gmail.com>
2025-08-19 23:07:23 +0800

    Add ENUM functionality
    Edited the commands to be of the a Enum type for future use and cleaner code. This allows for abstraction and allows for the Enums to be reused more easily in other parts of the project.

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+enum Command {
+    LIST, BYE, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT;
+    public static Command fromString(String command) {
+        try {
+            return Command.valueOf(command.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+    
+}
+
 public class Jeff {
     public static void main(String[] args) {
 
@@ -11,80 +23,84 @@ public class Jeff {
         );
 
         ArrayList<Task> tasks = new ArrayList<>();
+        boolean shouldBreak = false;
         
-        
-            while (sc.hasNextLine()) {
+            while (!shouldBreak && sc.hasNextLine()) {
                 try {
                     String input = sc.nextLine().trim();
                     String[] split = input.split("\\s+", 2);
-                    String cmd = split[0];
+                    Command cmd = Command.fromString(split[0]);
 
-                    if (cmd.equalsIgnoreCase("list")) {
-                        for (int i = 0; i < tasks.size(); i++) {
-                            if (tasks.get(i) != null) {
-                                System.out.println((i + 1) + ". " + tasks.get(i));
-                            }
-                        }
-                    } else if (cmd.equalsIgnoreCase("bye")) {
 
-                        break;
-
-                    } else if (split.length < 2) {
-
+                    
+                    if (cmd == null) {
                         throw new JeffException("EXCUSEEE MEEEE. THIS IS A INVALID COMMAND??!!! Try again.");
+                    }
 
-                    } 
-                        else if (cmd.equalsIgnoreCase("mark")) {
+                    switch (cmd) {
+                        case LIST:
+                            for (int i = 0; i < tasks.size(); i++) {
+                                if (tasks.get(i) != null) {
+                                    System.out.println((i + 1) + ". " + tasks.get(i));
+                                }
+                            }
+                            break;
+                        case BYE:
+                            
+                            shouldBreak = true;
+                            break;
 
-                        int length = Integer.parseInt(split[1]);
-                        tasks.get(length - 1).markAsDone();
-                        System.out.println("Task marked as done!");
-                        System.out.println(tasks.get(length - 1));
-                        System.out.println("______________________________");
+                        case MARK:
 
-                    } else if (cmd.equalsIgnoreCase("unmark")) {
-                        int length = Integer.parseInt(split[1]);
-                        tasks.get(length - 1).undo();
-                        System.out.println("Task marked as undone!");
-                        System.out.println(tasks.get(length - 1));
-                    } else if (cmd.equalsIgnoreCase("delete")) {
+                            int markIdx = Integer.parseInt(split[1]);
+                            tasks.get(markIdx - 1).markAsDone();
+                            System.out.println("Task marked as done!");
+                            System.out.println(tasks.get(markIdx - 1));
+                            System.out.println("______________________________");
+                            break;
+                        
+                        case UNMARK:
 
-                        int idx = (Integer.parseInt(split[1]) - 1); // This would be the index to be deleted.
+                            int unmarkIdx = Integer.parseInt(split[1]);
+                            tasks.get(unmarkIdx - 1).undo();
+                            System.out.println("Task marked as undone!");
+                            System.out.println(tasks.get(unmarkIdx - 1));
+                            break;
 
-                        if (idx < 0 || idx >= tasks.size()) {
-                            throw new JeffException("Invalid task number. Please try again.");
-                        }
-                        tasks.remove(idx);
-                        System.out.println("Task has been deleted.");
-                        System.out.println("You now have " + tasks.size() + " tasks in the list.");
+                        case DELETE:
 
+                            int idx = (Integer.parseInt(split[1]) - 1); // This would be the index to be deleted.
+
+                            if (idx < 0 || idx >= tasks.size()) {
+                                throw new JeffException("Invalid task number. Please try again.");
+                            }
+                            tasks.remove(idx);
+                            System.out.println("Task has been deleted.");
+                            System.out.println("You now have " + tasks.size() + " tasks in the list.");
+                            break;
+                        
+                        case TODO:
+                            tasks.add(new Todo(split[1]));
+                            added(input, tasks);
+                            break;
+
+                        case DEADLINE:
+                            String[] deadlineParts = split[1].split("/by", 2);
+                            tasks.add(new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim()));
+                            added(input, tasks);
+                            break;
+
+                        case EVENT:
+                            String[] eventParts = split[1].split("/at", 2);
+                            tasks.add(new Event(eventParts[0].trim(), eventParts[1].trim()));
+                            added(input, tasks);
+                            break;
                         
                     }
-                    else {
-                        
+                    
 
-                        switch (cmd.toLowerCase()) {
-                            case "todo":
-                                tasks.add(new Todo(split[1]));
-                                break;
-                            case "deadline":
-                                String[] deadlineParts = split[1].split("/by", 2);
-                                tasks.add(new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim()));
-                                break;
-                            case "event":
-                                String[] eventParts = split[1].split("/at", 2);
-                                tasks.add(new Event(eventParts[0].trim(), eventParts[1].trim()));
-                                break;
-                        
-                        }
-
-                        
-                        System.out.println("______________________________");
-                        System.out.println("Task has been added: " + input);
-                        System.out.println("You now have " + tasks.size() + " tasks in the list.");
-                        System.out.println("______________________________");
                     } 
-                }
+                
                 
 
             catch (JeffException e) {
@@ -95,5 +111,13 @@ public class Jeff {
         sc.close();
 
     }
+
+    private static void added(String input, ArrayList<Task> tasks) {
+        System.out.println("______________________________");
+        System.out.println("Task has been added: " + input);
+        System.out.println("You now have " + tasks.size() + " tasks in the list.");
+        System.out.println("______________________________");
+    }
+
 
 }
