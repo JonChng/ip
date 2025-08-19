23ad8b1b677e023f72d98ebbbc9800a62523af2f
JonChng <jonathanchngyk@gmail.com>
2025-08-19 16:43:00 +0800

    Update naming to "Jeff"

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,12 @@
+
+public class Jeff {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Jeff! Your own personal chatbot.\n");
+        System.out.println(
+            "What can I do for you?"
+        );
+        System.out.println("________________________________________");
+
+        System.out.println("Bye! Hope to you see you again soon!\n");
+    }
+}
