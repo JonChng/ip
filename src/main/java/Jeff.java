1281f05b0d24bba09dd0af88fba16cdcadae85b4
JonChng <jonathanchngyk@gmail.com>
2025-08-19 19:41:44 +0800

    Add Try/Catch

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -11,64 +11,74 @@ public class Jeff {
 
         Task[] tasks = new Task[100];
         int taskCount = 0;
+        
+        
+            while (sc.hasNextLine()) {
+                try {
+                    String input = sc.nextLine().trim();
+                    String[] split = input.split("\\s+", 2);
+                    String cmd = split[0];
 
-        while (sc.hasNextLine()) {
-            String input = sc.nextLine().trim();
-            String[] split = input.split("\\s+", 2);
-            String cmd = split[0];
+                    if (cmd.equalsIgnoreCase("list")) {
+                        for (int i = 0; i < tasks.length; i++) {
+                            if (tasks[i] != null) {
+                                System.out.println((i + 1) + ". " + tasks[i]);
+                            }
+                        }
+                    } else if (cmd.equalsIgnoreCase("bye")) {
+                        break;
+                    } else if (split.length < 2) {
+                        throw new JeffException("EXCUSEEE MEEEE. THIS IS A INVALID COMMAND??!!! Try again.");
+                    } 
+                        else if (cmd.equalsIgnoreCase("mark")) {
 
-            if (cmd.equalsIgnoreCase("list")) {
-                for (int i = 0; i < tasks.length; i++) {
-                    if (tasks[i] != null) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        int length = Integer.parseInt(split[1]);
+                        tasks[length - 1].markAsDone();
+                        System.out.println("Task marked as done!");
+                        System.out.println(tasks[length - 1]);
+                        System.out.println("______________________________");
+
+                    } else if (cmd.equalsIgnoreCase("unmark")) {
+                        int length = Integer.parseInt(split[1]);
+                        tasks[length - 1].undo();
+                        System.out.println("Task marked as undone!");
+                        System.out.println(tasks[length - 1]);
+                    } 
+                    else {
+                        if (taskCount < tasks.length) {
+
+                            switch (cmd.toLowerCase()) {
+                                case "todo":
+                                    tasks[taskCount] = new Todo(split[1]);
+                                    break;
+                                case "deadline":
+                                    String[] deadlineParts = split[1].split("/by", 2);
+                                    tasks[taskCount] = new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim());
+                                    break;
+                                case "event":
+                                    String[] eventParts = split[1].split("/at", 2);
+                                    tasks[taskCount] = new Event(eventParts[0].trim(), eventParts[1].trim());
+                                    break;
+                            
+                            }
+
+                            
+                            taskCount++;
+                            System.out.println("______________________________");
+                            System.out.println("Task has been added: " + input);
+                            System.out.println("You now have " + taskCount + " tasks in the list.");
+                            System.out.println("______________________________");
+                        } else {
+                            System.out.println("Task list is full!");
+                        }
                     }
-                }
-            } else if (cmd.equalsIgnoreCase("bye")) {
-                break;
-            } else if (cmd.equalsIgnoreCase("mark")) {
-
-                int length = Integer.parseInt(split[1]);
-                tasks[length - 1].markAsDone();
-                System.out.println("Task marked as done!");
-                System.out.println(tasks[length - 1]);
-                System.out.println("______________________________");
-
-            } else if (cmd.equalsIgnoreCase("unmark")) {
-                int length = Integer.parseInt(split[1]);
-                tasks[length - 1].undo();
-                System.out.println("Task marked as undone!");
-                System.out.println(tasks[length - 1]);
-            } 
-            else {
-                if (taskCount < tasks.length) {
-
-                    switch (cmd.toLowerCase()) {
-                        case "todo":
-                            tasks[taskCount] = new Todo(split[1]);
-                            break;
-                        case "deadline":
-                            String[] deadlineParts = split[1].split("/by", 2);
-                            tasks[taskCount] = new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim());
-                            break;
-                        case "event":
-                            String[] eventParts = split[1].split("/at", 2);
-                            tasks[taskCount] = new Event(eventParts[0].trim(), eventParts[1].trim());
-                            break;
-                       
-                    }
-
                     
-                    taskCount++;
-                    System.out.println("______________________________");
-                    System.out.println("Task has been added: " + input);
-                    System.out.println("You now have " + taskCount + " tasks in the list.");
-                    System.out.println("______________________________");
-                } else {
-                    System.out.println("Task list is full!");
-                }
-            }
 
-        }
+                }
+            catch (JeffException e) {
+                System.out.println(e.getMessage());
+            } 
+        } 
         System.out.println("Bye! Hope to you see you again soon!");
         sc.close();
 
