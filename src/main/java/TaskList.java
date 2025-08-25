2caf895ce4ce69de1a29c403cb9b9a27884fdac5
JonChng <jonathanchngyk@gmail.com>
2025-08-25 20:13:31 +0800

    Add TaskList

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList() { 
+
+        this.tasks = new ArrayList<>(); 
+
+    }
+    public TaskList(ArrayList<Task> existing) {
+
+         this.tasks = existing; 
+        
+    }
+
+    public void add(Task t) {
+        tasks.add(t);
+    }
+
+    public Task get(int i) {
+        return tasks.get(i);
+    }
+
+    public Task remove(int i) {
+        return tasks.remove(i);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+}
