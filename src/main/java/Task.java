60038cc347c9107626d5401d3d151df6099718d0
JonChng <jonathanchngyk@gmail.com>
2025-08-25 17:11:34 +0800

    Add local task saving
    Task saving functionality was added via Storage class.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,12 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String type;
 
-    public Task(String description) {
+    public Task(String description, String type) {
         this.description = description;
         this.isDone = false;
+        this.type = type;
     }
 
     public String getStatusIcon() {
@@ -17,6 +19,18 @@ public class Task {
 
     public void undo() {
         isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getType() {
+        return type;
     }
 
     @Override
