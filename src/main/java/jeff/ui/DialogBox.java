15c529b5a36f7c26aa33114db264c3ce4df7f9ee
JonChng <jonathanchngyk@gmail.com>
2025-09-02 12:01:28 +0800

    Update alignment

--- src/main/java/jeff/ui/DialogBox.java
+++ src/main/java/jeff/ui/DialogBox.java
@@ -1,5 +1,9 @@
 package jeff.ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,15 +16,31 @@ public class DialogBox extends HBox {
 
     public DialogBox(String s, Image i) {
         text = new Label(s);
+        text.setWrapText(true);
         displayPicture = new ImageView(i);
+        displayPicture.setFitWidth(40);
+        displayPicture.setFitHeight(40);
+        this.setSpacing(10);
         this.getChildren().addAll(text, displayPicture);
     }
 
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img);
+        db.setAlignment(Pos.TOP_RIGHT);
+        return db;
     }
 
     public static DialogBox getBotDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img);
+        db.flip();
+        db.setAlignment(Pos.TOP_LEFT);
+        return db;
     }
 }
