43fa39c49dfd96428b5315dfa131d9cd31d49b29
JonChng <jonathanchngyk@gmail.com>
2025-09-17 22:59:43 +0800

    Add AIAssisted test fixing

--- src/test/java/jeff/storage/StorageTest.java
+++ src/test/java/jeff/storage/StorageTest.java
@@ -2,21 +2,22 @@ package jeff.storage;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
 class StorageTest {
 
+    @Test
     void testLoad() {
-
         Storage storage = new Storage();
         ArrayList<String> result = storage.load();
-        assert result != null;
+        assertNotNull(result);
     }
 
+    @Test
     void testSaveAndLoad() {
-
         Storage storage = new Storage();
         ArrayList<String> testData = new ArrayList<>();
         testData.add("T|0|Test todo");
@@ -24,16 +25,16 @@ class StorageTest {
         storage.save(testData);
 
         ArrayList<String> loadedData = storage.load();
-        assert loadedData.contains("T|0|Test todo");
+        assertTrue(loadedData.contains("T|0|Test todo"));
     }
 
+    @Test
     void testSaveEmpty() {
-
         Storage storage = new Storage();
         ArrayList<String> emptyData = new ArrayList<>();
         storage.save(emptyData);
 
         ArrayList<String> loadedData = storage.load();
-        assert loadedData.size() == 0;
+        assertEquals(0, loadedData.size());
     }
 }
